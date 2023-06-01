195f898a2797dad07cef54de148aa530a924c710
Koteshdaram <koteshdaram735@gmail.com>
2023-06-01 17:22:04 +0530

    first commit

---
+++ Onlinebookshop-app/src/components/FooterComponent.js
@@ -0,0 +1,49 @@
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaTwitterSquare } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaInstagramSquare } from "react-icons/fa";
+import FooterHome from "./FooterHome";
+
+function FooterComponent(props) {
+    return (
+        <>
+         <FooterHome/>
+        <div
+            className={
+                props.position === "absolute"
+                    ? props.page === "Home"
+                        ? "footer footer-home footer-absolute"
+                        : "footer footer-absolute"
+                    : "footer"
+            }
+        >
+            <p className="copyright">@ 2023 Daram kotesh | Follow us My Website</p>
+            <ul className="footer-list">
+                <li className="footer-item">
+                    <a href="#" target="_blank" rel="noreferrer">
+                        <FaFacebookSquare />
+                    </a>
+                </li>
+                <li className="footer-item">
+                    <a href="#" target="_blank" rel="noreferrer">
+                        <FaTwitterSquare />
+                    </a>
+                </li>
+                <li className="footer-item">
+                    <a href="#" target="_blank" rel="noreferrer">
+                        <FaLinkedin />
+                    </a>
+                </li>
+                <li className="footer-item">
+                    <a href="#" target="_blank" rel="noreferrer">
+                        <FaInstagramSquare />
+                    </a>
+                </li>
+            </ul>
+        </div>
+        </>
+    
+    );
+}
+
+export default FooterComponent;
---
+++ Onlinebookshop-app/src/components/FooterHome.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import './FooterHome.css'
+
+function FooterHome() {
+  return (
+   <>
+<footer ClassName="">
+       <div>
+        <ul>
+            <li ClassName="footer-link-head">Links</li>
+            <li>Home</li>
+            <li>Books</li>
+        </ul>
+       </div>
+       <div>
+        <ul>
+            <li ClassName="footer-link-head">Contact</li>
+            <li>+8976543210</li>
+            <li>kotesh@gmail.com</li>
+        </ul>
+       </div>
+       <div>
+        <ul>
+            <li ClassName="footer-link-head">Address</li>
+            <li>Uppal,Hyderabad</li>
+            <li>kotesh@gmail.com</li>
+        </ul>
+       </div>
+        </footer>
+   </>
+  );
+}
+
+export default FooterHome
